7cdecd869aa155c5e6a0e537861093d7256b989d
Josh99 <joshuawisdom92@gmail.com>
2025-05-01 02:33:38 +0200

    Add Query import to fix bug

--- web_bff/main.py
+++ web_bff/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException, Request
+from fastapi import FastAPI, HTTPException, Request, Query  
 from fastapi.exceptions import RequestValidationError
 from fastapi.responses import PlainTextResponse, JSONResponse, Response
 import httpx
